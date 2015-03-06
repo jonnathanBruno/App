2740f9d001bde0a3b03ce365d04dbf2caa7ceaf7
JonnathanBruno <jonnathanbruno@gmail.com>
2015-03-06 11:25:36 -0300

    Primeiro commit

---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\Nudes\Desktop\ListaDeComprasAPK\app\platforms\android\ant-build\AndroidManifest.xml : \
+C:\Users\Nudes\Desktop\ListaDeComprasAPK\app\platforms\android\AndroidManifest.xml \
---
+++ platforms/android/ant-build/HelloCordova-debug-unaligned.apk.d
@@ -0,0 +1,5 @@
+C:\Users\Nudes\Desktop\ListaDeComprasAPK\app\platforms\android\ant-build\HelloCordova-debug-unaligned.apk : \
+C:\Users\Nudes\Desktop\ListaDeComprasAPK\app\platforms\android\ant-build\HelloCordova.ap_ \
+C:\Users\Nudes\Desktop\ListaDeComprasAPK\app\platforms\android\ant-build\classes.dex \
+C:\Users\Nudes\Desktop\ListaDeComprasAPK\app\platforms\android\CordovaLib\ant-build\classes.jar \
+C:\Users\Nudes\Desktop\ListaDeComprasAPK\app\platforms\android\libs\com.google.zxing.client.android.captureactivity.jar \
